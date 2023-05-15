ba0f236097e6519cab440be50d98a85e9db5fefc
David Pico <david.picomejuto@gmail.com>
2023-05-15 14:58:32 +0200

    Adding date format and timezone to response object

--- src/main/java/com/dpico/atsistemas/prices/application/response/PriceResponse.java
+++ src/main/java/com/dpico/atsistemas/prices/application/response/PriceResponse.java
@@ -1,5 +1,6 @@
 package com.dpico.atsistemas.prices.application.response;
 
+import com.fasterxml.jackson.annotation.*;
 import lombok.*;
 
 import java.util.*;
@@ -12,7 +13,9 @@ public class PriceResponse {
     private Long productId;
     private Long brandId;
     private Long priceList;
+    @JsonFormat(pattern="yyyy-MM-dd-HH.mm.ss", timezone="Europe/Madrid")
     private Date startDate;
+    @JsonFormat(pattern="yyyy-MM-dd-HH.mm.ss", timezone="Europe/Madrid")
     private Date endDate;
     private Float price;
 }
